9f1e0fa07fa0d6db69f35b1f22cc31d5f5dc547d
Thibaut <tfml1@st-andrews.ac.uk>
2021-11-16 15:59:39 +0100

    Rewrote the MPO for the chain displacement operator

--- include/chaindisplacementoperator.jl
+++ include/chaindisplacementoperator.jl
@@ -40,7 +40,7 @@ function polynomial(k,beta,n)
     end
 end
 
-function displacedchainmps(A::Vector{Any}, N::Int, Nm::Int; γ::Array{Complex{Float64},1}=nothing, chainparams, s=1, α=0.02, ωc=1, R=1, c_phonon=1, beta ="inf", issoft=false)
+function displacedchainmps(A::Vector{Any}, N::Int, Nm::Int; γ=nothing, chainparams=[fill(1.0,Nm),fill(1.0,Nm-1), 1.0], s=1, α=0.02, ωc=1, R=1, c_phonon=1, beta ="inf", issoft=false)
 """
 For a displacement gamma of the bath modes, compute the corresponding displaced operator on the 2*Nm-long chain and apply it to a given mps A.
 """
@@ -54,62 +54,38 @@ For a displacement gamma of the bath modes, compute the corresponding displaced 
     	bchain = (chainparams[2].^2)./ωc
     end
 
-    D = Any[] # argument of the Displacement operator as a MPO
     B = Any[] # displaced chain MPS
+    displ = Any[] # modulus square of the displacement amplitude
 
     # The system part of the MPS should be unaffected by the displacement operator
     for i=1:N
-        d1, d2, dsystem = size(A[i]) # left bond dim, right bond dim and system physical dim
-        χ = d1*d2 # bond dimension of the MPO
-        identity_system = zeros(ComplexF64,χ,χ,dsystem,dsystem)
-        #for j=1:d1
-            #for l=1:d2
-                identity_system[1,1,:,:] = unitmat(dsystem)
-            #end
-        #end
-        push!(D, identity_system)
-
-        Ap = reshape(A[i], χ*dsystem) #reshape the MPS tensor as a vector
-        M = reshape(identity_system, χ*dsystem, χ*dsystem) #reshape the MPO tensor as a square matrix
-
-        As, info = exponentiate(M,1,Ap) # we apply the displacement operator to the MPS tensor: B = exp(M)*A
-        if info.converged!=1
-            error("exponentiation did not converge")
-        end
-
-        push!(B, reshape(As, d1, d2, dsystem))
-
+        push!(B, A[i])
+        push!(displ, 0.0)
     end
 
     # Displacement operator for the chain
     for n=1:2*Nm
-        d1, d2, dhilbert = size(A[N+n])
+        d1, d2, dhilbert = size(A[N+n]) # left bond dim, right bond dim and system physical dim
         χ = d1*d2
-        W = zeros(ComplexF64,χ,χ,dhilbert,dhilbert) # Argument of the displacement operator
+        #W = zeros(ComplexF64,dhilbert,dhilbert) # Argument of the displacement operator
 
         bd = crea(dhilbert) # chain creation operator
         b = anih(dhilbert) # chain anihilation operator
 
-        ι = 0 #complex displacement amplitude
+        ι = 0 + 0.0im # complex displacement amplitude
         L = length(γ)
-
+        δk = 0.01 # spacing of the k-modes
         for k=1:L
             Unk = sqrt(spectraldensity(k/L))*polynomial(k/L,beta,n) # Matrix element of the unitary transformation from the bath to the chain
-            ι += γ[k]*Unk
+            ι += γ[k]*Unk*δk
         end
-        #for j=1:χ
-            #for l=1:χ
-                W[1,1,:,:] = ι*bd - conj(ι)*b
-            #end
-        #end
-        push!(D, W)
-        Ap = reshape(A[N+n], χ*dhilbert)
-        M = reshape(W, χ*dhilbert, χ*dhilbert)
+        push!(displ, abs(ι)^2)
 
-        As, info = exponentiate(M,1,Ap)
-        if info.converged!=1
-            error("exponentiation did not converge")
-        end
+        W = ι*bd - conj(ι)*b  # Argument of the displacement operator
+
+        Ap = permutedims(reshape(A[N+n], χ, dhilbert), [2,1]) # reshape the MPS tensor as a (d, χ)-matrix
+
+        As = permutedims(exp(W)*Ap, [2,1]) # matrix multiplication B_n = exp(W)*A_n
 
         push!(B, reshape(As, d1, d2, dhilbert))
     end
